d1e23285d7573c403cbe38b4ca6c508e8d131a16
Yo1o-sir <2406213396@qq.com>
2025-05-14 10:50:13 +0800

    Site updated: 2025-05-14 10:50:12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747190831407";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747191005102";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
